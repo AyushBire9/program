2b747eb4881bf8496768ef2962f56c1c58d28b3b
AyushBire9 <ayushbire123@gmail.com>
2025-02-20 13:26:30 +0530

    parimeter of rectangle

---
+++ perimeter of rectangle.py
@@ -0,0 +1,4 @@
+length=int(input("Enter the length of the rectangle: "))
+breadth=int(input("Enter the breadth of the rectangle: "))
+per=2*(length+breadth)
+print("Perimeter of the rectangle: ",per)
