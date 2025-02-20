180b7fe68e17a4f4f8a44c9f7ba1a595bf4aef65
AyushBire9 <ayushbire123@gmail.com>
2025-02-20 20:42:10 +0530

    Person is eligible for vote and licence or not

---
+++ check eligibility.py
@@ -0,0 +1,6 @@
+age=18
+person_age=int(input("Enter your age: "))
+if person_age>=age:
+    print("You are eligible to vote and driving licence .")
+else:
+    print("You are not eligible to vote and deiving licence.")
