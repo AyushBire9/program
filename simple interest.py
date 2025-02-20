22d1118a03321cab6508ed817d8719317ff5872e
AyushBire9 <ayushbire123@gmail.com>
2025-02-20 13:28:59 +0530

    simple interest

---
+++ simple interest.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter principal value:"))
+num2=int(input("Enter rate of intrest:"))
+num3=int(input("Enter the time in year:"))
+SI=(num1*num2*num3)/100
+print("The Simple Intrest is :",SI)
