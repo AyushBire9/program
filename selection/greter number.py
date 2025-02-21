6dbe8f71ed4ede5db9d449a70c9131c047715eaf
AyushBire9 <ayushbire123@gmail.com>
2025-02-21 10:12:21 +0530

    check greter number among three numbers

--- selection/greter number.py
+++ selection/greter number.py
@@ -5,5 +5,7 @@ if num1>num2 and num1>num3:
     print(num1," is the greatest number")
 elif num2>num1 and num2>num3:
     print(num2," is the greatest number")
-else:
+elif num3>num1 and num3>num2:
     print(num3," is the greatest number")
+else:
+    print("write diffrent numbers")
