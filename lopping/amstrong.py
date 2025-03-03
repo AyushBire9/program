147d19931d3c676c7bea20536be2f6e61ce83451
AyushBire9 <ayushbire123@gmail.com>
2025-03-03 21:10:19 +0530

    armstring number

--- lopping/amstrong.py
+++ lopping/amstrong.py
@@ -1,9 +1,10 @@
 num=int(input("Enter a number:"))
 arm=0
 temp=num
+l=len(str(num))
 while temp>0:
     digit=temp%10
-    arm=arm+digit**3
+    arm=arm+digit**l
     temp//=10
 if num==arm:
     print(num,"is an Armstrong number")
